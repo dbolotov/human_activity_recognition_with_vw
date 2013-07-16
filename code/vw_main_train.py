ddf9e8c9ae2a0a70c8455d512e5a9be9fba29128
dbolotov <dbolotov@live.com>
2013-07-15 21:15:25 -0700

    get correct prediction with least change to parameters

--- code/vw_main_train.py
+++ code/vw_main_train.py
@@ -9,24 +9,16 @@ import split
 import vw_evaluation
 
 ## Split into training and test sets
-split.main('../data/working/dataset_no_body_data.vw','train.vw','test.vw', 0.8, 'randy')
+split.main('../data/working/dataset.vw','../data/working/train.vw','../data/working/test.vw', 0.8, 'randy')
 
 ## Train vw model, save into file
-# cmd = "vw -d train_train.vw -f vw.model -c -k --loss_function logistic --passes 20 -q ee -b 27"
-
-cmd = "vw -d ../data/working/dataset.vw -f vw.model -c -k --oaa 5 -l 0.05 -q ff --passes 20"
-
-
-# cmd = "vw-varinfo -c -k --passes 10 --l1 0.0000001 train_train.vw > varinfo.txt"
-
+cmd = "vw -d ../data/working/dataset.vw -f ../data/output/vw.model -c -k --oaa 5 -l 0.05 --passes 5"
 os.system(cmd) 
 
-# sys.exit() 
 ## Predict using vw model and test set, save into file
-cmd = "vw -i vw.model -t test.vw -p vw_training_set_result.txt --quiet"
-os.system(cmd)
+os.system("vw -i ../data/output/vw.model -t ../data/working/test.vw -p ../data/working/test_pred.txt --quiet")
 
 ## Evaluate model results
-vw_evaluation.main('vw_training_set_result.txt','test.vw')
+vw_evaluation.main('../data/working/test_pred.txt','../data/working/test.vw')
 
 
