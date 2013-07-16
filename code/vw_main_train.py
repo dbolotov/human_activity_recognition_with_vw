607fbc82edfeaf1920ab2a7b525faed8696a2eb1
dbolotov <dbolotov@live.com>
2013-07-15 23:35:28 -0700

    testing

--- code/vw_main_train.py
+++ code/vw_main_train.py
@@ -11,7 +11,7 @@ import split,vw_evaluation
 split.main('../data/working/dataset.vw','../data/working/train.vw','../data/working/test.vw', 0.7, 'rand')
 
 ## Train vw model using train set (run as system cmd from python), save into file
-os.system("vw -d ../data/working/train.vw -f ../data/output/vw.model -c -k --oaa 5 -l 0.05 --passes 40") 
+os.system("vw -d ../data/working/train.vw -f ../data/output/vw.model -c -k --oaa 5 -l 0.05 --passes 40 --quiet") 
 
 ## Predict using vw model and test set, save into file
 os.system("vw -i ../data/output/vw.model -t ../data/working/test.vw -p ../data/working/test_pred.txt --quiet")
