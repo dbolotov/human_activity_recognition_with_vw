ab3063840d975c6c1f41eee96f31027e2bb18f11
dbolotov <dbolotov@live.com>
2013-07-15 20:32:01 -0700

    running experiments

--- code/vw_evaluation.py
+++ code/vw_evaluation.py
@@ -9,7 +9,7 @@ Use scikit-learn module for evaluation.
 
 import sys,csv
 import numpy as np
-from sklearn.metrics import accuracy_score, auc_score, roc_curve, auc, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn import metrics
 
 def main(y_pred_file,y_true_file):
@@ -17,21 +17,27 @@ def main(y_pred_file,y_true_file):
     ## Create list for predicted class
     lines = [line.strip() for line in open(y_pred_file)]
     y_pred_raw = [float(item) for item in lines]
-    y_pred = [-1 if item<0.0 else 1 for item in y_pred_raw]
+    y_pred = y_pred_raw
+    # y_pred = [-1 if item<0.0 else 1 for item in y_pred_raw]
+
+    # print y_pred_raw
+    # print y_pred
+    # sys.exit()
     
     
     ## Create list for true class
     lines = [line.strip() for line in open(y_true_file)]
-    y_true = [int(-1) if item[0]=='-' else int(1) for item in lines]
+    y_true = [float(item[0]) for item in lines]
+    # y_true = [int(-1) if item[0]=='-' else int(1) for item in lines]
     
     ## Evaluation
     acc = accuracy_score(y_true, y_pred)
-    auc = auc_score(y_true, y_pred)
+    # auc = auc_score(y_true, y_pred)
 
     cm = confusion_matrix(y_true,y_pred)
     print '\nModel Evaluation:\n'
     print '%-10s %12.5f' % ('accuracy:',acc)
-    print '%-10s %12.5f' % ('AUC:',auc)
+    # print '%-10s %12.5f' % ('AUC:',auc)
     print 'confusion matrix:'
     print cm
 
--- code/vw_main_train.py
+++ code/vw_main_train.py
@@ -9,12 +9,12 @@ import split
 import vw_evaluation
 
 ## Split into training and test sets
-split.main('../data/working/dataset.vw','train.vw','test.vw', 0.8, 'randy')
+split.main('../data/working/dataset_no_body_data.vw','train.vw','test.vw', 0.8, 'randy')
 
 ## Train vw model, save into file
 # cmd = "vw -d train_train.vw -f vw.model -c -k --loss_function logistic --passes 20 -q ee -b 27"
 
-cmd = "vw -d ../data/working/dataset.vw -f vw.model --csoaa 5"
+cmd = "vw -d ../data/working/dataset.vw -f vw.model -c -k --oaa 5 -l 0.05 -q ff --passes 20"
 
 
 # cmd = "vw-varinfo -c -k --passes 10 --l1 0.0000001 train_train.vw > varinfo.txt"
