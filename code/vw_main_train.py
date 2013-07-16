29d89258bac158aaa7b1decea532880da9d654eb
dbolotov <dbolotov@live.com>
2013-07-15 21:57:13 -0700

    cleaned up

--- code/vw_main_train.py
+++ code/vw_main_train.py
@@ -5,15 +5,13 @@ Driver script for building and evaluating vw models
 
 '''
 import os,sys
-import split
-import vw_evaluation
+import split,vw_evaluation
 
 ## Split into training and test sets
-split.main('../data/working/dataset.vw','../data/working/train.vw','../data/working/test.vw', 0.8, 'randy')
+split.main('../data/working/dataset.vw','../data/working/train.vw','../data/working/test.vw', 0.7, 'rnd')
 
-## Train vw model, save into file
-cmd = "vw -d ../data/working/dataset.vw -f ../data/output/vw.model -c -k --oaa 5 -l 0.05 --passes 5"
-os.system(cmd) 
+## Train vw model (run as system cmd from python), save into file
+os.system("vw -d ../data/working/dataset.vw -f ../data/output/vw.model -c -k --oaa 5 -l 0.05 --passes 5 --quiet") 
 
 ## Predict using vw model and test set, save into file
 os.system("vw -i ../data/output/vw.model -t ../data/working/test.vw -p ../data/working/test_pred.txt --quiet")
