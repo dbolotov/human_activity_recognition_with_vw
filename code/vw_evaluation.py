07f7884e91a89b5f5ffe8c3c52bd4ec85baf881f
dbolotov <dbolotov@live.com>
2013-07-16 00:41:49 -0700

    added classification report to output

--- code/vw_evaluation.py
+++ code/vw_evaluation.py
@@ -8,7 +8,7 @@ Usage: vw_evaluation.py <predicted_classes> <true_classes>
 
 import sys,csv
 import numpy as np
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
 def main(y_pred_file,y_true_file):
 
@@ -20,15 +20,19 @@ def main(y_pred_file,y_true_file):
     lines = [line.strip() for line in open(y_true_file)]
     y_true = [float(item[0]) for item in lines]
     
+    ## Define labels for classes
+    target_names = ['sitting', 'sittingdown', 'standing', 'standingup', 'walking']
     ## Evaluation
     acc = accuracy_score(y_true, y_pred)
     cm = confusion_matrix(y_true,y_pred)
+    cr = classification_report(y_true,y_pred,target_names=target_names)
 
     ## Print results
     print '\nModel Evaluation:\n'
     print '%-10s %12.5f' % ('accuracy:',acc)
     print 'confusion matrix:'
     print cm
+    print cr
 
 if __name__=='__main__':
     sys.exit(main(sys.argv[1],sys.argv[2]))
