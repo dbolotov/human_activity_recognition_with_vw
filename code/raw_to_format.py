cbd9033bca2ec6c58d66970f6e7a14eddd48b86e
dbolotov <dbolotov@live.com>
2013-07-16 23:44:49 -0700

    testing with --bfgs

--- code/raw_to_format.py
+++ code/raw_to_format.py
@@ -13,10 +13,9 @@ def main(in_file, out_file, format):
     reader = csv.reader(open(in_file), delimiter=";")
     writer = open(out_file,'w')
 
-    # namespaces = ['b1','b2','b3','b4','b5','b6','a7','a8','a9','a10','a11','a12','a13','a14','a15','a16','a17','a18']
+    namespaces = ['b1','b2','b3','b4','b5','b6','a7','a8','a9','a10','a11','a12','a13','a14','a15','a16','a17','a18']
     # namespaces = ['f1','f2','f3','f4','f5','f6','f7','f8','f9','f10','f11','f12','f13','f14','f15','f16'] #omit 'user' and 'gender'
     # namespaces = ['f1','f2','f3','f4','f5','f6','f7','f8','f9','f10','f11','f12'] #omit all but accel features
-    namespaces = ['b1']
 
     label_map = {'sitting':1, 'sittingdown': 2, 'standing': 3, 'standingup': 4, 'walking': 5}
 
@@ -27,7 +26,6 @@ def main(in_file, out_file, format):
         counter = 1
         for line in reader: 
             # line = line[15:] #use [2:] to omit the 'user' and 'gender' features
-            line = [line[1]] + [line[-1]]
             new_line = []
 
             label = label_map[line[-1]]
@@ -37,10 +35,7 @@ def main(in_file, out_file, format):
 
             for n in namespaces:
                 item = line.pop( 0 )
-                if n == 'b1' or n == 'b2':
-                    new_item = "|%s _%s" % ( n, item ) #add underscore for hashing categorical features
-                else:
-                    new_item = "|%s %s" % ( n, item )
+                new_item = "|%s %s" % ( n, item )
                 # if n == 'b1':
                 #     new_item = "example_%s|%s _%s" % ( counter, n, item ) #add example number
                 # else:
